3f1b02c827711410ea897939013e92da7bbe8daf
Gupta Aman <guptaaman@192.168.1.18>
2022-07-17 20:55:09 +0530

    changed

--- Building-a-Simple-Chatbot-in-Python-using-NLTK-master/chatbot.py
+++ Building-a-Simple-Chatbot-in-Python-using-NLTK-master/chatbot.py
@@ -67,11 +67,11 @@ def response(user_response):
         robo_response = robo_response+sent_tokens[idx]
         return robo_response
  
-
+ 
 flag=True
 print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
 while(flag==True):
-    user_response = input() 
+    user_response = input()  
     user_response=user_response.lower()
     if(user_response!='bye'):
         if(user_response=='thanks' or user_response=='thank you' ):
@@ -84,7 +84,7 @@ while(flag==True):
                 print("ROBO: ",end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
-    else:
+    else: 
         flag=False
         print("ROBO: Bye! take care..")    
         
